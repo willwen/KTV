c555da44d19322c47e2abb84baa8e27f0da0a2b5
Will Wen <will.wen1@gmail.com>
2017-06-18 22:26:57 -0400

    refactored and made a const.js, fix timings for song 2

---
+++ webpage/const.js
@@ -0,0 +1,19 @@
+const searchBarID = "songSearchInput";
+const resultsListID = "resultsList";
+
+
+const pinyinCheckBoxID = "pinyinCB";
+const pinyinLyricsLineClass = "pinyinLine";
+const cnCharCheckBoxID = "cnCB";
+const cnCharLyricsLineClass = "cnLine";
+const englishCheckBoxID = "engCB";
+const englishLyricsLineClass = "engLine";
+
+const titleLineID = "titleLine"
+
+const lyricsBodyID = "lyricsBody";
+
+const genericLinePrefix = "line";
+
+
+const audioPlayerID = "audioPlayer";
--- webpage/init.js
+++ webpage/init.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	//set a hook that runs every time a audio player steps
 	addTimeUpdateListener();
 
-	window.addEventListener('keydown', function(e) {
+	window.addEventListener("keydown", function(e) {
 	  if(e.keyCode == 32 && e.target == document.body) {
 		toggleAudioPlayer(); // space bar to toggle audio player
 		e.preventDefault(); // and prevent scrolling
@@ -10,117 +10,125 @@ $(document).ready(function(){
 	});
 
 
-	$('#songSearchInput').keyup(function(){
-		$('#resultsList').css('display', 'inline')
-		httpPostAsync("query", {search: $('#songSearchInput').val()}, showResults);
+	$("#" + searchBarID).keyup(function(){
+		$("#" + resultsListID).css("display", "inline");
+		httpPostAsync("query", {search: $("#" + searchBarID).val()}, showResults);
     });
+
+    setCheckBoxListeners();
 
 });
 
+function setCheckBoxListeners(){
 
-//set space bar listener to pause/play audio
-function addSpaceBarPlayPauseHook(){
-	$(window).keypress(function(e) {
-	    if (e.which === 32) {
-	    	toggleAudioPlayer();
-	    }
+	$("#" + pinyinCheckBoxID).change(function(){
+		var pinyin_cb = this;
+		$("." + pinyinLyricsLineClass).each(function(i, obj){
+			if(pinyin_cb.checked)
+				$(obj).show();
+			else
+				$(obj).hide();
+		});
 	});
 
+	$("#" + cnCharCheckBoxID).change(function(){
+		var cn_cb = this;
+		$("." + cnCharLyricsLineClass).each(function(i, obj){
+			if(cn_cb.checked)
+				$(obj).show();
+			else
+				$(obj).hide();
+		});
+	});
+	$("#" + englishCheckBoxID).change(function(){
+	var en_cb = this;
+	$("." + englishLyricsLineClass).each(function(i, obj){
+		if(en_cb.checked)
+			$(obj).show();
+		else
+			$(obj).hide();
+	});
+});
 }
+// //set space bar listener to pause/play audio
+// function addSpaceBarPlayPauseHook(){
+// 	$(window).keypress(function(e) {
+// 	    if (e.which === 32) {
+// 	    	toggleAudioPlayer();
+// 	    }
+// 	});
 
-function removeSpaceBarPlayPauseHook(){
-	$(window).unbind("keypress");
+// }
 
-}
-function simpleSearch(){
-    var input, filter, ul, li, a, i;
-    input = document.getElementById("songSearchInput");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-
-        }
-    }
-}
-
-function queryResult(responseObj){
-	alert(JSON.parse(responseObj));
-
-}
 //toggle play/pause on the audio player
 function toggleAudioPlayer(){
-	$("#audioPlayer").prop("paused") ? $("#audioPlayer").trigger("play") : $("#audioPlayer").trigger("pause");
+	$("#" + audioPlayerID).prop("paused") ? $("#" + audioPlayerID).trigger("play") : $("#" + audioPlayerID).trigger("pause");
 }
 
 function updateAudioPlayer(sourceUrl){
-	$("#audioPlayer").attr("src", sourceUrl);
+	$("#" + audioPlayerID).attr("src", sourceUrl);
 	return;
 }
 
 //function to skip audio player to certain time period
 function skipToTime(lineNumber){
-	return '$("#audioPlayer").prop("currentTime",' + timestampToSeconds(times[lineNumber]) + ')';
-
+	return "$('#" + audioPlayerID + "').prop('currentTime'," + timestampToSeconds(times[lineNumber]) + ")";
 }
+
 //global array to that maps line number (the index of the array)
 //to the timestamp of the song
 var times;
 
 //when received a response from server, update div with payload
 function updateDiv(responseObj){
-	$('#lyricsBody').empty();
-	var isEng = $('#eng').is(':checked');
-	var isChar = $('#cn').is(':checked');
-	var isPinYin = $('#pinyin').is(':checked');
-	// var resp = JSON.parse(responseObj);
+	$("#" + lyricsBodyID).empty();
+	var isEng = $("#" + englishCheckBoxID).is(":checked");
+	var isChar = $("#" + cnCharCheckBoxID).is(":checked");
+	var isPinYin = $("#" + pinyinCheckBoxID).is(":checked");
+
 	var eng = responseObj.engArray;
 	var cnChar = responseObj.cnArray;
 	var pinyin = responseObj.pinyinArray;
 	times = responseObj.timesArray;
+
 	var lineNumber = 0;
 	for (i in eng){
-		var divBody = "<div id ='line" + lineNumber+ "'>";
+		var divBody = "<div id ='"+ genericLinePrefix + lineNumber + "'>";
 		if(isPinYin)
-			divBody += pinyin[i]+ "<br/>";
-		if(isChar){
-			divBody += '<span id = "char' + lineNumber + '"">'
-				+ cnChar[i] + "</span>" +"<br/>";
-		}
+			divBody += "<div class= '" + pinyinLyricsLineClass + "'>" + pinyin[i]+ "</div>";
+		if(isChar)
+			divBody += "<div class = '" + cnCharLyricsLineClass + "'>" + cnChar[i] + "</div>";
 		if(isEng)
-			divBody += eng[i] + "<br/>"
-		$("#lyricsBody").append(divBody + "</div><br/>");
-		if(isChar){
-			$("#char" + lineNumber).dblclick(function (){
-				skipToTime(i);
-				$("#audioPlayer").trigger("play"); 
-			});
-			$("#char" + lineNumber).hover(function (){
-				Math.floor(times[i]/100) + ':' + times[i]%100;
-			});
-		}
+			divBody += "<div class = '" +englishLyricsLineClass + "'>" + eng[i] + "</div>";
+
+		
+		$("#" + lyricsBodyID).append(divBody + "</div><br/>");
+		// if(isChar){
+		// 	$("#char" + lineNumber).dblclick(function (){
+		// 		skipToTime(i);
+		// 		$("#audioPlayer").trigger("play"); 
+		// 	});
+		// 	$("#char" + lineNumber).hover(function (){
+		// 		Math.floor(times[i]/100) + ':' + times[i]%100;
+		// 	});
+		// }
 		lineNumber++;
 	}
 	updateAudioPlayer(responseObj.songPath);
 	hideResultsList();
 }
 function hideResultsList(){
-	$('#resultsList').css('display', 'none')
+	$("#" + resultsListID).css('display', 'none')
 }
 function showResults(responseObj){
-	$("#resultsList").empty();
+	$("#" + resultsListID).empty();
 	responseObj.forEach(function(element){
-		var html = '<li><button class = "button list-group-item list-group-item-action" id = ' + element.file_name + '>'
-		+ element.cn_char +' - '+ element.artist + '</span></button></li>';
-		$("#resultsList").append(html);
+		var html = "<li><button class = 'button list-group-item list-group-item-action' id = " + element.file_name + ">"
+		+ element.cn_char +" - "+ element.artist + "</span></button></li>";
+		$("#" + resultsListID).append(html);
 		$("#" + element.file_name).click(function(){
-			$("#title").text(element.cn_char + "-" + element.artist);
-			httpGetAsync('song', {id: element.file_name}, updateDiv);
+			$("#" + titleLineID).text(element.cn_char + " - " + element.artist);
+			httpGetAsync("song", {id: element.file_name}, updateDiv);
 			return false;
 		});
 	})
@@ -157,20 +165,20 @@ function updateLine(){
 		currentLine++;
 		return;
 	}
-	var time = Math.round($("#audioPlayer").prop("currentTime"));
+	var time = Math.round($("#" + audioPlayerID).prop("currentTime"));
 	
 	if (time >= convertedToSeconds)
 	{
-		$("#line" + (currentLine - 2)).css('color','#000000')
-		$("#line" + (currentLine - 1)).css('color','#000000')
-		$("#line" + currentLine).css('color','#66d9ef')
+		$("#" + genericLinePrefix + (currentLine - 2)).css('color','#000000')
+		$("#" + genericLinePrefix + (currentLine - 1)).css('color','#000000')
+		$("#" + genericLinePrefix + currentLine).css('color','#66d9ef')
 		// $("#line" + (currentLine + 1)).css('color','#A7C520')
 		currentLine++;
 	}
 }
 
 function addTimeUpdateListener(){
-	var temp = $("#audioPlayer");
+	var temp = $("#" + audioPlayerID);
 	temp.on('timeupdate', updateLine);
 }
 
