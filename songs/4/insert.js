1410647320fcea25dfb92c12dcdbfe35f703ea51
Will Wen <will.wen1@gmail.com>
2017-06-21 17:59:00 -0400

    added 2 new songs

---
+++ songs/4/insert.js
@@ -0,0 +1,15 @@
+db.songs.insert({    
+	"title_pinyin": "lanlianhua",
+    "cn_char": "蓝莲花",
+    "file_name": "4",
+    "artist": "许巍",
+    "artist_pinyin": "xuwei",
+    "searchTerm": "lanlianhua 蓝莲花 许巍 xuwei"},
+    {
+	    "title_pinyin": "cengjingdeni",
+	    "cn_char": "曾经的你",
+	    "file_name": "1",
+	    "artist": "许巍",
+	    "artist_pinyin": "xuwei",
+	    "searchTerm": "cengjingdeni 曾经的你 许巍 xuwei"
+    })
