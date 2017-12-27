5e1ad4977b503d05b1943ae44fccb4e1d4d15f29
Sherry Mei <sherrymei95@gmail.com>
2017-12-26 23:58:13 -0500

    Fixed updatetimestamp error

--- webpage/timepicker/Lyrics.jsx
+++ webpage/timepicker/Lyrics.jsx
@@ -1,71 +1,69 @@
 import $ from 'jquery'
 
 export default class Lyrics extends React.Component {
-	constructor(){
-		super();
-		this.state = {
-			timestamps: []
-		}
-		this.handleChange = this.handleChange.bind(this)	
-		this.updateValue = this.updateValue.bind(this)
-	}
-	componentWillMount(){
-	  	
-	}
-	componentDidMount(){
-	   
-	}
-	componentWillUnmount(){
-	
-	}
+    constructor() {
+        super();
+        this.state = {
+            values: []
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.updateValue = this.updateValue.bind(this)
+    }
+    componentWillMount() {
 
+    }
+    componentDidMount() {
 
+    }
+    componentWillUnmount() {
 
-	handleChange (e, index) {
-		const timestampFormat = /[0-9]:[0-5][0-9]/;
-		if (timestampFormat.exec(e.target.value)){
-	         this.props.updateTimestamp(index, e.target.value);
-	         $("#lineID" + index).css("background-color", "white");
-	    }
-	    else {
-	     	$("#lineID" + index).css("background-color", "red");
-	    }
     }
 
-    updateValue(e, index) {
-        let timestamps = this.props.timestamps[index];
-        timestamps.push(e.target.value);
-      }
-	
-	render() {
-		return (
+    handleChange(e, index) {
+        const timestampFormat = /[0-9]:[0-5][0-9]/;
+        if (timestampFormat.exec(e.target.value)) {
+            this.props.updateTimestamp(index, e.target.value);
+            $("#lineID" + index).css("background-color", "white");
+        } else {
+            $("#lineID" + index).css("background-color", "red");
+        }
+    }
 
-            <div className="container dotted-border" id="lyrics">
-	            {
-	            	this.props.lyrics.map((line, index) =>{
-	            		let realLineNum = index + 1;
-	            		let className = (this.props.arrayIndex == index) ? "line row currentLine" : "line row";
-		            	return (<div key = {index} className = {className}>
-		            		<div className = "lineNumber col-3">{realLineNum}</div> 
-		            		<div id = {realLineNum} className = "lineText col-8">
-		            			{(this.props.arrayIndex == index)? "→  " : ""}
-		            			{line}
-	            			</div>
-		            		<div id = {realLineNum + 'timestamp'} className = "timestamp col-1">
-		            		<input className = "timestampInput"
-		            			id = {'lineID' + index}
-		            			type="text"
-		            			value= {this.props.timestamps[index] ? this.props.timestamps[index] : ""}
-		            			onBlur={(e)  => this.handleChange(e, index)}
-		            			onChange={(e) => this.updateValue(e, index)}
-		            			maxLength="4"
-		            			size="4"></input>
-		            		</div>
-		            	</div>)
-
-		           	})
-	            }
-            </div>
-		);
+   	updateValue(event, index){
+		let values = this.state.values;
+		values[index] = event.target.value;
+		this.setState({values: values});
 	}
+
+    render() {
+        return (
+            <div className = "container dotted-border" id = "lyrics" > 
+            	{this.props.lyrics.map((line, index) => {
+                    let realLineNum = index + 1;
+                    let className = (this.props.arrayIndex == index) ? "line row currentLine" : "line row";
+                    this.state.values = this.props.timestamps;
+                    return ( 
+                    	<div key = {index} className = {className} >
+                        	<div className = "lineNumber col-3" > { realLineNum } </div>  
+                        	<div id = { realLineNum } className = "lineText col-8" > 
+                        		{(this.props.arrayIndex == index) ? "→  " : "" }{line} 
+                        	</div> 
+                        	<div id = { realLineNum + 'timestamp' } className = "timestamp col-1" > 
+	                        	<input className = "timestampInput"
+							        	id = {'lineID' + index}
+							        	type="text"
+							        	value= {this.state.values[index] ? this.state.values[index] : ''}
+							        	onBlur={(e)  => this.handleChange(e, index)}
+							        	onChange={(e) => this.updateValue(e,index)}
+							        	maxLength="4"
+							        	size="4">
+	        					</input> 
+                            </div> 
+                        </div>
+                    )
+                })} 
+            </div>
+        );
+    }
 }
+
