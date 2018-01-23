bbfe0788ae97715a50431b97e908492f837e34a5
Will Wen <will.wen1@gmail.com>
2018-01-23 10:45:46 -0500

    temp for cert sign

--- server.js
+++ server.js
@@ -35,7 +35,13 @@ environment === "production" ?
 
 //Express Setup//////////////////////////////////////////////////////////////////////////////////////////////////
 var app = express();
-
+app.get('/', (req, res)=>{
+    res.send("saV2Z_NmBhtyrn_xQ6KD3JAdf6n-s0sw7qebxL8o58s.zPZRJbebpojmPKLyveLzPkJD9vA1zMX6tOONhhCecag")
+})
+var server = app.listen(process.env.PORT || port, function() {
+    console.log('Listening on port %s!', server.address().port)
+})
+return;
 app.use(express.static('webpage/index'))
 app.use(express.static('webpage/submit'))
 app.use(express.static('webpage/song'))
