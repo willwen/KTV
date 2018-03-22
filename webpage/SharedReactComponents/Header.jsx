a563948119af18403b090f8b6b619a878d2f21d4
Will Wen <will.wen1@gmail.com>
2018-03-22 14:47:20 -0400

    small front page edits

--- webpage/SharedReactComponents/Header.jsx
+++ webpage/SharedReactComponents/Header.jsx
@@ -10,24 +10,23 @@ export default class PageHeader extends React.Component {
       <div className = "half-height">
        <div className = "header_img">
             <div className = "header_text">
-              <span className = "border">
-                <div className="welcome">
-                  <h1>Become a Karaoke Legend</h1>
-                  <h2>
-                    A Website to Learn Foreign Songs<br />by Will Wen
-                  </h2>
-                  <a
-                    href="https://github.com/willwen/KTV"
-                    target="_blank"
-                    className="fa fa-github"
-                  />
-                  <a
-                    href="https://www.linkedin.com/in/will-wen-52480559/"
-                    target="_blank"
-                    className="fa fa-linkedin"
-                  />
-                </div>
-              </span>
+            <div className="welcome">
+              <h1>Become a Karaoke Legend</h1>
+              <h2>
+                A Website to Learn Foreign Songs<br />by Will Wen
+              </h2>
+              <a
+                href="https://github.com/willwen/KTV"
+                target="_blank"
+                className="fa fa-github"
+              />
+              <a
+                href="https://www.linkedin.com/in/will-wen-52480559/"
+                target="_blank"
+                className="fa fa-linkedin"
+              />
+            </div>
+
             </div>
         </div>
       </div>
